45c3f91bbf50274d57599d168300d60028c19870
dt3280097@gmail.com <dt3280097@gmail.com>
2024-05-26 20:26:05 +0545

    edidted the api & create the button

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,47 +1,46 @@
-import { BottomNavigation } from '@mui/material';
-import '../css/blog.css';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import React, { useState } from 'react';
+import '../css/blog.css';
 
 const Blog = () => {
   const [data, setData] = useState([]);
+  const [visibleData, setVisibleData] = useState([]);
+  const [showAll, setShowAll] = useState(false);
+  const [limit, setLimit] = useState(5); // Limit the number of articles shown initially
 
   const getNews = () => {
     axios.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=c7ad30435c994ec8b2f7ef9984405da9")
       .then((response) => {
-        console.log(response.data.articles);
         setData(response.data.articles);
+        setVisibleData(response.data.articles.slice(0, limit));
       })
       .catch((error) => {
-        console.error('Error fetching the news:', error);
+        console.error("API did not fetch", error);
       });
   };
 
+  useEffect(() => {
+    getNews();
+  }, []);
+
+  const handleViewAll = () => {
+    setShowAll(true);
+    setVisibleData(data);
+  };
+
   return (
-    <>
-      <div className='blog_container'>
-        <button className='btn btn-primary' onClick={getNews}>Click for News</button>
-      </div>
-      <div className='card-grid'>
-        {
-          data.map((value, index) => (
-            <div className='flip-card' key={index}>
-              <div className='flip-card-inner'>
-                <div className='flip-card-front'>
-                  <img src={value.urlToImage} alt="news" style={{ width: '300px', height: '300px' }} />
-                </div>
-                <div className='flip-card-back'>
-                  <h4>{value.title}</h4>
-                  <p>{value.description}</p>
-                  <button>view all</button>
-                </div>
-              </div>
-            </div>
-          ))
-        }
-      </div>
-    </>
-  )
+    <div className='card_container'>
+      {visibleData.map((value, index) => (
+        <div className='card_main' key={index}>
+          <img src={value.urlToImage} alt='news' style={{ width: '300px', height: '300px' }} />
+          <h2>{value.title}</h2>
+          <p>{value.description}</p>
+          <button className='btn btn-more'>Read more</button>
+        </div>
+      ))}
+      {!showAll && <button className='btn btn-all' onClick={handleViewAll}>View all</button>}
+    </div>
+  );
 }
 
 export default Blog;
