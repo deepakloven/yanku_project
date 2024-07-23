22a42d1d0be88907a9eff4466df16ca2a078e951
dt3280097@gmail.com <dt3280097@gmail.com>
2024-07-23 12:19:48 +0545

    error page not found a

--- src/index.js
+++ src/index.js
@@ -1,34 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Navbar from './components/Navbar';
 import About from './components/About';
 import Services from './components/Services';
-import Footer from './components/Footer';
 import Main from './components/Main';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import News from './components/News';
-// import App from './App';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <Router>
-  <Navbar/>
-  <Routes>
-    <Route path='/main' element={<Main/>}/>
-    <Route path='/about' element={<About/>}/>
-    <Route path='/services' element={<Services/>}/>
-     <Route path='/news' element={<News/>}/>
-    {/* <Route path='/contact' element={<Contact/>}/>  */}
-    </Routes>
-    {/* <Services/> */}
-    {/* <App/> */}
-    {/* <Footer/> */}
+  <React.StrictMode>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/main" element={<Main />} /> {/* Default route */}
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/news" element={<News />} />
+        {/* <Route path="/contact" element={<Contact />} /> */}
+      </Routes>
     </Router>
-    
+  </React.StrictMode>
 );
 
 reportWebVitals();
